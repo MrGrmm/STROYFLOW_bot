3d102426a037f8012fc0ab177678326d92380e57
Gramma <gramma1997ion@gmail.com>
2023-03-11 03:35:14 +0200

    Izmenil keyboards'i

--- keyboards.py
+++ keyboards.py
@@ -1,80 +1,153 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-# Клавиатура главного меню
-
+                # kb Клавиатура главного меню
 kb_main_menu = ReplyKeyboardMarkup(resize_keyboard=True)
 
-btn_mm_help = KeyboardButton(text='Помощь')
-btn_mm_description = KeyboardButton(text='Описание')
-btn_mm_call = KeyboardButton(text='Запросить звонок', request_contact=True)
+btn_mm_call = KeyboardButton(text='📞 Запросить звонок', request_contact=True)
+btn_mm_o_nas = KeyboardButton(text='О нас')
+btn_mm_oformiti_zakaz = KeyboardButton(text='✅ Оформить заказ')
 
-kb_main_menu.add(btn_mm_help, btn_mm_description).add(btn_mm_call)
-
-# Клавиатура главной страницы
-ikb_oficial_page = InlineKeyboardMarkup(inline_keyboard=True)
-
-btn_op_catalog = InlineKeyboardButton(text="Каталог", callback_data='catalog')
-btn_op_our_works = InlineKeyboardButton(text="Наши работы", callback_data='our_works')
-btn_op_buy = InlineKeyboardButton(text="Где купить", callback_data='where_to_buy')
-btn_op_company = InlineKeyboardButton(text="Компания", callback_data='company')
-btn_op_contacts = InlineKeyboardButton(text="Контакты", callback_data='contacts')
-
-ikb_oficial_page.add(btn_op_catalog, btn_op_our_works, btn_op_buy, btn_op_company, btn_op_contacts) 
-
-# Клавиатура каталога
-ikb_catalog = InlineKeyboardMarkup()
-
-btn_catalog_rev_luk = InlineKeyboardButton(text="Ревизионный люк", callback_data='rev_luk')
-btn_catalog_san_uzel = InlineKeyboardButton(text="Сантехнический узел", callback_data="san_uzel")
-btn_catalog_mm = InlineKeyboardButton(text="Главное меню", callback_data="mm")
-
-ikb_catalog.add(btn_catalog_rev_luk, btn_catalog_san_uzel).add(btn_catalog_mm)
-
-# Клавиатура ревизионный люк
-ikb_rev_luk = InlineKeyboardMarkup()
-
-btn_rl_odnost = InlineKeyboardButton(text="Одностворчатый люк",callback_data="odnost")
-btn_rl_dvust = InlineKeyboardButton(text="Двустворчатый люк", callback_data="dvust")
-btn_rl_gobrazn = InlineKeyboardButton(text="Г-образный люк", callback_data="gobrazn")
-btn_rl_pobrazn = InlineKeyboardButton(text="П-образный люк", callback_data="pobrazn")
-btn_rl_podvannu = InlineKeyboardButton(text="Люки под ванну", callback_data="podvannu")
-btn_rl_lukdver = InlineKeyboardButton(text="Люки-двери", callback_data="lukdver")
-btn_rl_back = InlineKeyboardButton(text="Назад", callback_data="back_to_catalog")
-btn_rl_mm = InlineKeyboardButton(text="Главное меню", callback_data="mm")
-
-ikb_rev_luk.add(btn_rl_odnost, btn_rl_dvust).add(btn_rl_gobrazn, btn_rl_pobrazn).add(btn_rl_podvannu, btn_rl_lukdver).add(btn_rl_back, btn_rl_mm)
-
-# Клавиатура выбора своего люка
-
-ikb_view_buy = InlineKeyboardMarkup()
-
-btn_vb_v1 = InlineKeyboardButton(text='Вариант №1', callback_data="odn_v1")
-btn_vb_v2 = InlineKeyboardButton(text='Вариант №2', callback_data="odn_v2")
-btn_vb_v3 = InlineKeyboardButton(text='Вариант №3', callback_data="odn_v3")
-btn_vb_v4 = InlineKeyboardButton(text='Вариант №4', callback_data="odn_v4")
-btn_vb_v5 = InlineKeyboardButton(text='Вариант №5', callback_data="odn_v5")
-btn_vb_v6 = InlineKeyboardButton(text='Вариант №6', callback_data="odn_v6")
-btn_vb_back = InlineKeyboardButton(text='Назад', callback_data="back_to_rev_luk")
-btn_vb_mm = InlineKeyboardButton(text='Главное меню', callback_data="mm")
-
-ikb_view_buy.add(btn_vb_v1, btn_vb_v2, btn_vb_v3).add(btn_vb_v4, btn_vb_v5, btn_vb_v6).add(btn_vb_back, btn_vb_mm)
-
-# Клавиатура презентации одностворчатого люка варианта 1
-
-ikb_odnost_v1 = InlineKeyboardMarkup()
-
-btn_odn_v1_video = InlineKeyboardButton(text='Видео 3D-проект', callback_data='odn_v1_video')
-btn_odn_v1_photos = InlineKeyboardButton(text='Фото готовой работы', callback_data='odn_v1_photos')
-btn_odn_v1_buy = InlineKeyboardButton(text='Оформить заказ', callback_data='odn_v1_buy')
-btn_odn_v1_back = InlineKeyboardButton(text='Назад', callback_data="back_to_view_buy")
-btn_odn_v1_mm = InlineKeyboardButton(text='Главное меню', callback_data="mm")
-
-ikb_odnost_v1.add(btn_odn_v1_video, btn_odn_v1_photos).add(btn_odn_v1_buy).add(btn_odn_v1_back, btn_odn_v1_mm)
+kb_main_menu.add(btn_mm_o_nas).add(btn_mm_call).insert(btn_mm_oformiti_zakaz)
 
 
-ikb_next_buy = InlineKeyboardMarkup()
+                # Клавиатура главной страницы
+ikb_main_menu = InlineKeyboardMarkup(row_width=2)
 
-btn_next_buy = InlineKeyboardButton(text='Продолжить покупки', callback_data='next_buy')
+btn_imm_rev_luk = InlineKeyboardButton(text="Ревизионный люк", callback_data='rev_luk')
+btn_imm_san_uzel = InlineKeyboardButton(text="Сантехнический узел", callback_data="san_uzel")
 
-ikb_next_buy.add(btn_next_buy)
+ikb_main_menu.add(btn_imm_rev_luk).add(btn_imm_san_uzel)
+
+
+                # Клавиатура FAQ   
+ikb_faq = InlineKeyboardMarkup(row_width=2)
+
+btn_faq_faq = InlineKeyboardButton(text='Частые вопросы', callback_data='faq')
+btn_faq_vopros = InlineKeyboardButton(text="Задать свой вопрос", callback_data="zadati_vopros")
+btn_faq_back = InlineKeyboardButton(text='Назад в главное меню', callback_data='mm')
+
+ikb_faq.add(btn_faq_faq, btn_faq_vopros).add(btn_faq_back)
+
+
+
+                # Клавиатура оформить заказ
+ikb_ready_to_buy = InlineKeyboardMarkup(row_width=1)
+
+btn_rtb_buy = InlineKeyboardButton(text='Купить!', callback_data='buy')
+btn_rtb_udal_iz_korz = InlineKeyboardButton(text='Убрать товар из корзины', callback_data='udal_iz_korz')
+btn_rtb_back = InlineKeyboardButton(text='Назад в главное меню', callback_data='mm')
+
+ikb_ready_to_buy.add(btn_rtb_buy).add(btn_rtb_udal_iz_korz).add(btn_rtb_back)
+
+
+
+                # Клавиатура Ревизионный люк
+ikb_rev_luke = InlineKeyboardMarkup(row_width=3)
+
+btn_rl_last_var = InlineKeyboardButton(text='👈🏼', callback_data='last_var')
+btn_rl_next_var = InlineKeyboardButton(text='👉🏼', callback_data='next_var')
+btn_rl_vibrati = InlineKeyboardButton(text='Выбрать категорию', callback_data='vibrati')
+btn_rl_back = InlineKeyboardButton(text='Назад в главное меню', callback_data='mm')
+btn_rl_recomand = InlineKeyboardButton(text='Рекомендации по подготовке проёма', callback_data='recomanded')
+btn_rl_faq = InlineKeyboardButton(text='Вопрос/Ответ', callback_data='faq_ikb')
+
+ikb_rev_luke.add(btn_rl_last_var, btn_rl_vibrati, btn_rl_next_var).add(btn_rl_back).add(btn_rl_recomand).add(btn_rl_faq)
+
+
+
+
+                # Клавиатура Люки маленьких размеров
+ikb_small_luke = InlineKeyboardMarkup(row_width=3)
+
+btn_sl_200x200 = InlineKeyboardButton(text='200x200', callback_data='200x200')
+btn_sl_200x300 = InlineKeyboardButton(text='200x200', callback_data='200x300')
+btn_sl_200x400 = InlineKeyboardButton(text='200x200', callback_data='200x400')
+btn_sl_200x500 = InlineKeyboardButton(text='200x200', callback_data='200x500')
+btn_sl_300x200 = InlineKeyboardButton(text='200x200', callback_data='300x200')
+btn_sl_400x200 = InlineKeyboardButton(text='200x200', callback_data='400x200')
+btn_sl_500x200 = InlineKeyboardButton(text='200x200', callback_data='500x200')
+btn_sl_300x300 = InlineKeyboardButton(text='200x200', callback_data='300x300')
+btn_sl_300x400 = InlineKeyboardButton(text='200x200', callback_data='300x400')
+btn_sl_podtverditi = InlineKeyboardButton(text='Подтвердить выбор', callback_data='podtverditi')
+btn_sl_back = InlineKeyboardButton(text='Назад', callback_data='back_to_rl')
+btn_sl_mm = InlineKeyboardButton(text='Главное меню', callback_data='mm')
+
+ikb_small_luke.add(btn_sl_200x200, btn_sl_200x300, btn_sl_200x400).add(btn_sl_200x500, btn_sl_300x200, btn_sl_400x200)
+ikb_small_luke.add(btn_sl_500x200, btn_sl_300x300, btn_sl_300x400).add(btn_sl_podtverditi).add(btn_sl_back, btn_sl_mm)
+
+
+
+
+                # Клавиатура Люки средних размеров
+ikb_midle_luke = InlineKeyboardMarkup(row_width=3)
+
+btn_ml_400x400 = InlineKeyboardButton(text='200x200', callback_data='400x400')
+btn_ml_400x500 = InlineKeyboardButton(text='200x200', callback_data='400x500')
+btn_ml_400x600 = InlineKeyboardButton(text='200x200', callback_data='400x600')
+btn_ml_500x400 = InlineKeyboardButton(text='200x200', callback_data='500x400')
+btn_ml_500x500 = InlineKeyboardButton(text='200x200', callback_data='500x500')
+btn_ml_500x600 = InlineKeyboardButton(text='200x200', callback_data='500x600')
+btn_ml_600x400 = InlineKeyboardButton(text='200x200', callback_data='600x400')
+btn_ml_600x500 = InlineKeyboardButton(text='200x200', callback_data='600x500')
+btn_ml_600x600 = InlineKeyboardButton(text='200x200', callback_data='600x600')
+btn_ml_podtverditi = InlineKeyboardButton(text='Подтвердить выбор', callback_data='podtverditi')
+btn_ml_back = InlineKeyboardButton(text='Назад', callback_data='back_to_rl')
+btn_ml_mm = InlineKeyboardButton(text='Главное меню', callback_data='mm')
+
+ikb_midle_luke.add(btn_ml_400x400, btn_ml_400x500, btn_ml_400x600).add(btn_ml_500x400, btn_ml_500x500, btn_ml_500x600)
+ikb_midle_luke.add(btn_ml_600x400, btn_ml_600x500, btn_ml_600x600).add(btn_ml_podtverditi).add(btn_ml_back, btn_ml_mm)
+
+
+
+
+                # Клавиатура Люки больших размеров
+ikb_big_luke = InlineKeyboardMarkup(row_width=3)
+
+btn_bl_700x500 = InlineKeyboardButton(text='200x200', callback_data='700x500')
+btn_bl_700x600 = InlineKeyboardButton(text='200x200', callback_data='700x600')
+btn_bl_700x700 = InlineKeyboardButton(text='200x200', callback_data='700x700')
+btn_bl_500x700 = InlineKeyboardButton(text='200x200', callback_data='500x700')
+btn_bl_500x800 = InlineKeyboardButton(text='200x200', callback_data='500x800')
+btn_bl_500x900 = InlineKeyboardButton(text='200x200', callback_data='500x900')
+btn_bl_600x700 = InlineKeyboardButton(text='200x200', callback_data='600x700')
+btn_bl_600x800 = InlineKeyboardButton(text='200x200', callback_data='600x800')
+btn_bl_600x900 = InlineKeyboardButton(text='200x200', callback_data='600x900')
+btn_bl_podtverditi = InlineKeyboardButton(text='Подтвердить выбор', callback_data='podtverditi')
+btn_bl_back = InlineKeyboardButton(text='Назад', callback_data='back_to_rl')
+btn_bl_mm = InlineKeyboardButton(text='Главное меню', callback_data='mm')
+
+ikb_midle_luke.add(btn_bl_700x500, btn_bl_700x600, btn_bl_700x700).add(btn_bl_500x700, btn_bl_500x800, btn_bl_500x900)
+ikb_midle_luke.add(btn_bl_600x700, btn_bl_600x800, btn_bl_600x900).add(btn_bl_podtverditi).add(btn_bl_back, btn_bl_mm)
+
+
+
+
+                # Клавиатура Выбрать вариант
+ikb_vibrati_var = InlineKeyboardMarkup(row_width=3)
+
+btn_vv_3d_project = InlineKeyboardButton(text='3D Проект', callback_data='3d_project')
+btn_vv_chertej = InlineKeyboardButton(text='Чертёж', callback_data='chertej')
+btn_vv_gotovie_raboti = InlineKeyboardButton(text='Готовые работы', callback_data='gotovie_raboti')
+btn_vv_recomand = InlineKeyboardButton(text='Рекомендации по подготовке проёма', callback_data='recomanded')
+btn_vv_dobaviti_v_korzinu = InlineKeyboardButton(text='Добавить в корзину')
+btn_vv_back = InlineKeyboardButton(text='Назад', callback_data='back_to_rl')
+btn_vv_mm = InlineKeyboardButton(text='Главное меню', callback_data='mm')
+
+ikb_vibrati_var.add(btn_vv_3d_project, btn_vv_chertej, btn_vv_gotovie_raboti)
+ikb_vibrati_var.add(btn_vv_recomand).add(btn_vv_dobaviti_v_korzinu).add(btn_vv_back, btn_vv_mm)
+
+                # Клавиатура Убрать товар
+
+#TODO Создать эту клавиатуру внутри хэндлера с тем количеством кнопок скольок товаров добавил пользователь в корзину
+
+
+                # Клавиатура Купить
+ikb_buy = InlineKeyboardMarkup(row_width=1)
+
+btn_b_otpraviti_zakaz = InlineKeyboardButton(text='Отправить заказ', callback_data='otpraviti_zakaz')
+btn_b_recomand = InlineKeyboardButton(text='ВАЖНО! Подготовка проёма', callback_data='recomanded')
+btn_b_back = InlineKeyboardButton(text='Назад', callback_data='back_to_rtb')
+btn_b_mm = InlineKeyboardButton(text='Главное меню', callback_data='mm')
+
+ikb_buy.add(btn_b_otpraviti_zakaz).add(btn_b_recomand).add(btn_b_back, btn_b_mm)
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from aiogram import Dispatcher, Bot, types, executor
 from aiogram.dispatcher.filters import Text
 from aiogram.types import ReplyKeyboardRemove
-from keyboards import ikb_oficial_page, kb_main_menu, ikb_catalog, ikb_rev_luk, ikb_view_buy, ikb_odnost_v1, ikb_next_buy
+from keyboards import kb_main_menu, ikb_faq, ikb_main_menu, ikb_ready_to_buy
 from config import TOKEN_API
 
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
+
+# images = ['https://i.pinimg.com/originals/8a/de/fe/8adefe5af862b4f9cec286c6ee4722cb.jpg', 'https://img3.akspic.ru/previews/7/4/2/8/6/168247/168247-kosti_3d-igra_v_kosti_3d-azartnaya_igra-pitevaya_igra-kazino-500x.jpg', 'https://img1.akspic.ru/previews/7/4/7/9/6/169747/169747-ikanvas-art-pechat_na_holste-poster-oblako-500x.jpg' , 'https://mobimg.b-cdn.net/v3/fetch/94/94c56e15f13f1de4740a76742b0b594f.jpeg']
 
 last_message_id = None
 last_photo_id = None
@@ -17,42 +19,45 @@ HELP_COMMAND = '''
 <b>/help</b> - <em>Запуск бота</em>'''
 
 DESCRIPTION = '''
-Предназначение бота/Реклама'''
+Информация о комании SanFlow, продуктах что продаём и производителей продуктов'''
+
+
+
+
 
 
 async def start_on(_):
     print('Бот запущен!!')
 
-@dp.message_handler(Text(equals='Помощь'))
-async def send_mess_help(message: types.Message):
-    await message.answer(text=HELP_COMMAND, parse_mode='HTML')
-    await message.delete()
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    # Отправляем сообщение с клавиатурой
+    global last_message_id, last_photo_id
+    with open('media\photo_2023-03-09_22-37-54.jpg', 'rb') as photo_logo:
+        await bot.send_photo(message.chat.id, photo=photo_logo, reply_markup=kb_main_menu)
+    last_message_id = (await bot.send_message(message.chat.id, "Вас приветствует компания SanFlow выберите продукт который вас интересует", reply_markup=ikb_main_menu)).message_id
+    
 
 @dp.message_handler(Text(equals='Запросить звонок'))
 async def send_mess_help(message: types.Message):
     await message.answer("Пожалуйста, поделитесь своим контактом")
     await message.delete()
 
-@dp.message_handler(Text(equals='Описание'))
+@dp.message_handler(Text(equals='📞 О нас'))
 async def send_mess_help(message: types.Message):
     await message.answer(text=DESCRIPTION, parse_mode='HTML')
     await message.delete()
 
-# Обработчик команды /start
-# @dp.message_handler(commands=['start'])
-# async def send_welcome(message: types.Message):
-#     # Отправляем сообщение с клавиатурой
-#     await bot.send_photo(message.chat.id, photo='https://downloader.disk.yandex.ru/preview/ae17ce9ecd2337ab5e8aca813940c449e9caa43890c761c99211fcba596c9d0f/63fe8e37/SOj6rJU1FTkMHZkELIev4ykJd90JxsyXat_vYJllQ_XaeAwbietxLsTimN6PL3I_Q2fkQCMQUSQEg0PskNtJhg%3D%3D?uid=0&filename=2022-05-31%2016-08-29.PNG&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=0&tknv=v2&size=976x882', reply_markup=kb_main_menu)
-#     await bot.send_message(message.chat.id, "Выберите раздел:", reply_markup=ikb_oficial_page)
-    
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    # Отправляем сообщение с клавиатурой
-    global last_message_id, last_photo_id
-    photo = 'https://downloader.disk.yandex.ru/preview/ae17ce9ecd2337ab5e8aca813940c449e9caa43890c761c99211fcba596c9d0f/63fe8e37/SOj6rJU1FTkMHZkELIev4ykJd90JxsyXat_vYJllQ_XaeAwbietxLsTimN6PL3I_Q2fkQCMQUSQEg0PskNtJhg%3D%3D?uid=0&filename=2022-05-31%2016-08-29.PNG&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=0&tknv=v2&size=976x882'
-    await bot.send_photo(message.chat.id, photo=photo, reply_markup=kb_main_menu)
-    last_message_id = (await bot.send_message(message.chat.id, "Выберите раздел:", reply_markup=ikb_oficial_page)).message_id
+@dp.message_handler(Text(equals='Вопрос/Ответ'))
+async def send_mess_help(message: types.Message):
+    await bot.delete_message(chat_id=message.chat.id, message_id=last_message_id)
+    await message.answer(text='Меню FAQ:', reply_markup=ikb_faq)
+    await message.delete()
 
+
+    
 @dp.message_handler(content_types=types.ContentType.TEXT, regexp='сантехника')
 async def handle_santekhnika(message: types.Message):
     # отправляем ответное сообщение
@@ -69,52 +74,37 @@ async def process_callback_button(callback_query: types.CallbackQuery):
     await bot.delete_message(callback_query.from_user.id, last_message_id)
     if last_photo_id is not None:
         await bot.delete_message(callback_query.from_user.id, last_photo_id)
-    # Отправляем сообщение с текстом, соответствующим нажатой кнопке
-    if callback_query.data == 'catalog':
-        last_message = await bot.send_message(callback_query.from_user.id, "Вы выбрали раздел 'Каталог'", reply_markup=ikb_catalog)
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'our_works':
-        last_message = await bot.send_message(callback_query.from_user.id, "Вы выбрали раздел 'Наши работы'")
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'where_to_buy':
-        last_message = await bot.send_message(callback_query.from_user.id, "Вы выбрали раздел 'Где купить'")
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'company':
-        last_message = await bot.send_message(callback_query.from_user.id, "Вы выбрали раздел 'Компания'")
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'contacts':
-        last_message = await bot.send_message(callback_query.from_user.id, "Вы выбрали раздел 'Контакты'")
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'rev_luk':
+#     # Отправляем сообщение с текстом, соответствующим нажатой кнопке
+    if callback_query.data == 'rev_luk':
         last_message = await bot.send_photo(callback_query.from_user.id, photo='https://shag-ma.ru/upload/medialibrary/111/c9l9ag1tnp5cpqwu2xk3fwdmh4epgdwa.jpg', reply_markup=ikb_rev_luk)
         last_message_id = last_message.message_id
-    elif callback_query.data == 'odnost':
-        last_message = await bot.send_photo(callback_query.from_user.id, photo='https://shag-ma.ru/upload/medialibrary/424/p0qvzyetlcrim1x5vxywlvqnlapjrzyn.jpg', reply_markup=ikb_view_buy)
+#     elif callback_query.data == 'odnost':
+#         last_message = await bot.send_photo(callback_query.from_user.id, photo='https://shag-ma.ru/upload/medialibrary/424/p0qvzyetlcrim1x5vxywlvqnlapjrzyn.jpg', reply_markup=ikb_view_buy)
+#         last_message_id = last_message.message_id
+#     elif callback_query.data == 'odn_v1':
+#         last_message = await bot.send_photo(callback_query.from_user.id, photo='https://lh3.googleusercontent.com/unz6g-pk_83GHr4Z85m6CCe2vtlH7mLM_bZZKzfVfLksQ4O9FroZm0TtDnP5UMHM8vxW_7fu_PObd6QsIpTXzVRIXJHU37YkdFZFN6hkua6HUv0nBpQTFl4DGIGFvb-RYPBmQzwfdqMiRIh2pkCj7y-TSuuOlueezfFh5xe2I2yqiWLxE6DWqr1m9WsERmzGctRQdiju0WqOlT2hpULBc9MyH4Ay1uEVc8WE4L14LxgPRZzDAnKoAGG9lvR9W7mt9D1GfnEGhuJo82uRby1s3d2cA1eQKHCzQyZpcAolJB9n9taqr4lPGClOT8FIcsxEXEvCBzc9ITCXiyugcct5qrqf8dfW1ZzIxvUwWQ_ZtzYl92jjQIRcPf7iYoq1ka7ShcQ3BxZT6RsNg81T4Ot2ZITgy1UBONYmFPsiQR9unJXBDQPDvzdpiaPYSHLRkZwmMujGr2TStZYo_CRI-jTaS2n05VqCeHCeqSM5PrjZjFMdqpOd84U0ZJRTdvSGNVqvdP5QgwV1Ka4DSzQd0PoTPWBu4UEpNxYhqcmgVWgoQ-JTDPoMxVtyDrp-9MlJZK-1_OvdpMj6RLNsht8Np6jG78PrKvxs_vkA7FdS14cDJHMrciBn5tgELOt_TzD2quCKFeTtoHhM_Asen6jGuU91NEaeiGBNnyVL4NokzASAuBU5Z1kE_3ub9zQGt5TE3vF-EiXeLwZGq1KCGc5gcC8ITLCxiEwNNhYQmSlNTZLJSUkuFm4GhfWo580lEe7fodUMmYDczhOP2l2aenvYlsKC804GysK4BCqAP9-hMzBjB6BqgTqlEG8pmSH0TC5skfBPfzytBzfP0-4NSpeijKYh9Q1HYWDNIEAKduk2HVMDNPg8T6iDkfOem4suvPDsjaMRsRXtN2JjQE5DVxWe6PjYSSYAtnE4Ai6DDWQIbOqain2i=w597-h624-no?authuser=0', caption='Вы выбрали Вариант №1', reply_markup=ikb_odnost_v1)
+#         last_message_id = last_message.message_id
+#     elif callback_query.data == 'odn_v1_photos':
+#         last_message = await bot.send_photo(callback_query.from_user.id, photo='https://downloader.disk.yandex.ru/preview/f04629fdf8ed2f03a3efffc91a09e829cccee32edf7718b3c51138873bf160e0/63fecb37/KYY58Jh4gtgzz5899_Rgtlc4B7G3mQW9exTtvI-Xj3H3UhaSiYVLRYL9uIab7T5YViMFP1WtEAnm_tbWxJjOLw%3D%3D?uid=0&filename=2022-05-11%2021-19-14.JPG&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=997x882', caption='Вы выбрали Вариант №1', reply_markup=ikb_odnost_v1)
+#         last_message_id = last_message.message_id
+    elif callback_query.data == 'faq':
+        last_message = await bot.send_message(callback_query.from_user.id, "Вы перешли в меню 'FAQ'", reply_markup=ikb_faq)
         last_message_id = last_message.message_id
-    elif callback_query.data == 'odn_v1':
-        last_message = await bot.send_photo(callback_query.from_user.id, photo='https://lh3.googleusercontent.com/unz6g-pk_83GHr4Z85m6CCe2vtlH7mLM_bZZKzfVfLksQ4O9FroZm0TtDnP5UMHM8vxW_7fu_PObd6QsIpTXzVRIXJHU37YkdFZFN6hkua6HUv0nBpQTFl4DGIGFvb-RYPBmQzwfdqMiRIh2pkCj7y-TSuuOlueezfFh5xe2I2yqiWLxE6DWqr1m9WsERmzGctRQdiju0WqOlT2hpULBc9MyH4Ay1uEVc8WE4L14LxgPRZzDAnKoAGG9lvR9W7mt9D1GfnEGhuJo82uRby1s3d2cA1eQKHCzQyZpcAolJB9n9taqr4lPGClOT8FIcsxEXEvCBzc9ITCXiyugcct5qrqf8dfW1ZzIxvUwWQ_ZtzYl92jjQIRcPf7iYoq1ka7ShcQ3BxZT6RsNg81T4Ot2ZITgy1UBONYmFPsiQR9unJXBDQPDvzdpiaPYSHLRkZwmMujGr2TStZYo_CRI-jTaS2n05VqCeHCeqSM5PrjZjFMdqpOd84U0ZJRTdvSGNVqvdP5QgwV1Ka4DSzQd0PoTPWBu4UEpNxYhqcmgVWgoQ-JTDPoMxVtyDrp-9MlJZK-1_OvdpMj6RLNsht8Np6jG78PrKvxs_vkA7FdS14cDJHMrciBn5tgELOt_TzD2quCKFeTtoHhM_Asen6jGuU91NEaeiGBNnyVL4NokzASAuBU5Z1kE_3ub9zQGt5TE3vF-EiXeLwZGq1KCGc5gcC8ITLCxiEwNNhYQmSlNTZLJSUkuFm4GhfWo580lEe7fodUMmYDczhOP2l2aenvYlsKC804GysK4BCqAP9-hMzBjB6BqgTqlEG8pmSH0TC5skfBPfzytBzfP0-4NSpeijKYh9Q1HYWDNIEAKduk2HVMDNPg8T6iDkfOem4suvPDsjaMRsRXtN2JjQE5DVxWe6PjYSSYAtnE4Ai6DDWQIbOqain2i=w597-h624-no?authuser=0', caption='Вы выбрали Вариант №1', reply_markup=ikb_odnost_v1)
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'odn_v1_photos':
-        last_message = await bot.send_photo(callback_query.from_user.id, photo='https://downloader.disk.yandex.ru/preview/f04629fdf8ed2f03a3efffc91a09e829cccee32edf7718b3c51138873bf160e0/63fecb37/KYY58Jh4gtgzz5899_Rgtlc4B7G3mQW9exTtvI-Xj3H3UhaSiYVLRYL9uIab7T5YViMFP1WtEAnm_tbWxJjOLw%3D%3D?uid=0&filename=2022-05-11%2021-19-14.JPG&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=997x882', caption='Вы выбрали Вариант №1', reply_markup=ikb_odnost_v1)
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'back_to_catalog':
-        last_message = await bot.send_message(callback_query.from_user.id, "Вы перешли в меню 'Каталог'", reply_markup=ikb_catalog)
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'back_to_rev_luk':
-        last_message = await bot.send_message(callback_query.from_user.id, "Вы перешли в меню 'Ревизионный люк'", reply_markup=ikb_rev_luk)
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'back_to_view_buy':
-        last_message = await bot.send_message(callback_query.from_user.id, "Вы перешли в меню 'Ревизионный люк'", reply_markup=ikb_view_buy)
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'mm':
-        last_message = await bot.send_message(callback_query.from_user.id, "Вы перешли в Главное меню", reply_markup=ikb_oficial_page)
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'odn_v1_buy':
-        last_message = await bot.send_message(callback_query.from_user.id, "Здесь будет опросник (Формат ещё обдумывается)", reply_markup=ikb_next_buy)
-        last_message_id = last_message.message_id
-    elif callback_query.data == 'next_buy':
-        last_message = await bot.send_message(callback_query.from_user.id, "Вы перешли в меню 'Каталог'", reply_markup=ikb_catalog)
-        last_message_id = last_message.message_id
+#     elif callback_query.data == 'back_to_rev_luk':
+#         last_message = await bot.send_message(callback_query.from_user.id, "Вы перешли в меню 'Ревизионный люк'", reply_markup=ikb_rev_luk)
+#         last_message_id = last_message.message_id
+#     elif callback_query.data == 'back_to_view_buy':
+#         last_message = await bot.send_message(callback_query.from_user.id, "Вы перешли в меню 'Ревизионный люк'", reply_markup=ikb_view_buy)
+#         last_message_id = last_message.message_id
+#     elif callback_query.data == 'mm':
+#         last_message = await bot.send_message(callback_query.from_user.id, "Вы перешли в Главное меню", reply_markup=ikb_oficial_page)
+#         last_message_id = last_message.message_id
+#     elif callback_query.data == 'odn_v1_buy':
+#         last_message = await bot.send_message(callback_query.from_user.id, "Здесь будет опросник (Формат ещё обдумывается)", reply_markup=ikb_next_buy)
+#         last_message_id = last_message.message_id
+#     elif callback_query.data == 'next_buy':
+#         last_message = await bot.send_message(callback_query.from_user.id, "Вы перешли в меню 'Каталог'", reply_markup=ikb_catalog)
+#         last_message_id = last_message.message_id
     else:
         pass
 
